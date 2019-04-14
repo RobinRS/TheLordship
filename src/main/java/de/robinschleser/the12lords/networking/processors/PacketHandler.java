3e179a9acbfa8b6ae024da855974901d5f09ee77
Robin <robinschleser0608@gmail.com>
2019-04-14 13:30:36 +0200

    Networking started

--- src/main/java/de/robinschleser/the12lords/networking/processors/PacketHandler.java
+++ src/main/java/de/robinschleser/the12lords/networking/processors/PacketHandler.java
@@ -6,16 +6,10 @@ import de.robinschleser.the12lords.networking.packets.PingPacket;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-
 public class PacketHandler extends SimpleChannelInboundHandler<Packet> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Packet in) throws Exception {
-        InetSocketAddress socketAddress = (InetSocketAddress) ctx.channel().remoteAddress();
-        InetAddress inetaddress = socketAddress.getAddress();
-        String ipAddress = inetaddress.getHostAddress();
         if (in instanceof PingPacket) {
             System.out.println("Current Ping: " + (System.currentTimeMillis() - Starter.pingSend + " MS.") );
             Thread.sleep(500);
